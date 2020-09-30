3fe20b196588cd8105dd9679729fd8f90f351b76
Cairo Flores <florescairo2902@gmail.com>
2020-09-30 09:18:56 -0700

    public folder files added

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+class App extends React.Component {
+    // STATE
+
+    // FUNCTIONS
+
+    // RENDER
+    render = () => {
+        return (
+            <div>
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
