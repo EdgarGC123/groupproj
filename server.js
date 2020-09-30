9337d72fc87de8e5faaa17dae36608dce58c560f
Edgar Galvan-Cuesta <edgargalvan-cuesta@Edgars-MacBook-Pro-2.local>
2020-09-30 10:47:37 -0500

    finished server.js except contollers

--- server.js
+++ server.js
@@ -14,3 +14,27 @@ app.use(express.static("public"))
 
 
 //connection
+const MONGODB_URI = process.env.MONGODB_URI
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+
+
+//error / success
+mongoose.connection.on('error', err =>
+console.log(
+    err.message,
+    ' is Mongod not running?/Problem with Atlas Connection?'
+)
+)
+mongoose.connection.on('connected', () =>
+console.log('mongo connected: ', MONGODB_URI)
+)
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+
+//LISTENER
+app.listen(PORT, ()=>{
+    console.log("listening on port", PORT)
+})
